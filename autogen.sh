7766eec5d37e12d867423fd1edc5e2fbb07be62a
Taylor Petrick <taylor@sidefx.com>
2016-08-29 10:17:15 -0400

    Updated autogen.sh to point to GTest's Github releases instead of Google Code. The Google Code archive link now 404s.

--- autogen.sh
+++ autogen.sh
@@ -19,8 +19,10 @@ fi
 # directory is set up as an SVN external.
 if test ! -e gtest; then
   echo "Google Test not present.  Fetching gtest-1.5.0 from the web..."
-  curl http://googletest.googlecode.com/files/gtest-1.5.0.tar.bz2 | tar jx
-  mv gtest-1.5.0 gtest
+  curl -k -L -O https://github.com/google/googletest/archive/release-1.5.0.zip
+  unzip -q release-1.5.0.zip
+  rm release-1.5.0.zip
+  mv googletest-release-1.5.0 gtest
 fi
 
 set -ex
